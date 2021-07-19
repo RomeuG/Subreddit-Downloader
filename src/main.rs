473772540619500e88c4d265b89459af9781da2e
Romeu Vieira <romeu.bizz@gmail.com>
2021-07-19 18:47:29 +0100

    Use Arc<RwLock<T>>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
 use std::error::Error;
+use std::sync::Arc;
+// use std::sync::RwLock;
+use parking_lot::RwLock;
+use reqwest::Client;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 struct AccessToken {
@@ -33,7 +37,7 @@ struct JsonThreadData {
 }
 
 #[derive(Debug)]
-struct Reddit {
+pub struct Reddit {
     id: String,
     secret: String,
     user: String,
@@ -69,183 +73,232 @@ impl Reddit {
         }
     }
 
-    fn check_sleep(&self) {
-        println!(
-            "Remaining requests: {} | Reset seconds: {}",
-            self.remaining_requests, self.reset_seconds
-        );
-
-        if self.remaining_requests == 0 {
-            println!("Sleeping for {} seconds...", self.reset_seconds);
-            std::thread::sleep(std::time::Duration::from_secs(self.reset_seconds as u64));
-        }
-    }
-
-    fn parse_headers(&mut self, response: &reqwest::blocking::Response) {
-        self.remaining_requests = response
-            .headers()
-            .get("x-ratelimit-remaining")
-            .unwrap_or(&reqwest::header::HeaderValue::from_str("0.0").unwrap())
-            .to_str()
-            .unwrap_or("0.0")
-            .to_string()
-            .parse::<f32>()
-            .unwrap_or(0.0) as i32;
-
-        self.used_requests = response
-            .headers()
-            .get("x-ratelimit-used")
-            .unwrap_or(&reqwest::header::HeaderValue::from_str("0").unwrap())
-            .to_str()
-            .unwrap_or("0")
-            .to_string()
-            .parse::<i32>()
-            .unwrap_or(0);
-
-        self.reset_seconds = response
-            .headers()
-            .get("x-ratelimit-reset")
-            .unwrap_or(&reqwest::header::HeaderValue::from_str("0").unwrap())
-            .to_str()
-            .unwrap_or("0")
-            .to_string()
-            .parse::<i32>()
-            .unwrap_or(0);
-    }
-
-    pub async fn req_threads(&mut self, outdir: &String, thread_list: Vec<String>) {
-        let header_auth = format!(
-            "Bearer {}",
-            self.access_token.as_ref().unwrap().access_token
-        );
-
-        for thread in thread_list {
-            self.get_thread(outdir, &thread, &header_auth).await;
-        }
-    }
-
-    async fn get_thread(&mut self, outdir: &String, thread_name: &String, header_auth: &String) {
-        self.check_sleep();
-
-        let url_fmt = format!("https://oauth.reddit.com{}.json", thread_name);
-        let url = reqwest::Url::parse(&url_fmt).unwrap();
-
-        let client = reqwest::blocking::Client::new();
-        let res = client
-            .get(url.clone())
-            .header(reqwest::header::USER_AGENT, &self.user_agent)
-            .header(reqwest::header::AUTHORIZATION, header_auth)
-            .send()
-            .unwrap();
-
-        println!("Downloaded {}", url);
-
-        let full_path = generate_full_path(outdir.clone(), get_thread_name(&thread_name));
-        println!("Full path: {}", full_path);
-
-        self.parse_headers(&res);
-
-        let response_json: serde_json::Value = res.json().unwrap();
-        let pretty_printed = serde_json::to_string_pretty(&response_json).unwrap();
-
-        save_to_file(&full_path, &pretty_printed);
-    }
-
-    pub fn req_access_token(&mut self) {
-        let url_fmt = format!("https://www.reddit.com/api/v1/access_token?grant_type=password&username={}&password={}", self.user, self.pass);
-        let url = reqwest::Url::parse(&url_fmt).unwrap();
-
+    pub fn get_basic_token(&self) -> String {
         let auth_plain = format!("{}:{}", self.id, self.secret);
         let auth_b64 = base64::encode(auth_plain);
-        let header_auth = format!("Basic {}", auth_b64);
 
-        let client = reqwest::blocking::Client::new();
-        let res = client
-            .post(url)
-            .header(reqwest::header::USER_AGENT, &self.user_agent)
-            .header(reqwest::header::AUTHORIZATION, &header_auth)
-            .send()
-            .unwrap();
-
-        let json = res.json::<AccessToken>().unwrap();
-
-        self.access_token = Some(json.clone());
+        format!("Basic {}", auth_b64)
     }
 
-    pub fn req_subreddit(&self) -> Vec<String> {
-        let mut thread_url_list: Vec<String> = vec![];
-
-        self.check_sleep();
-
-        let url_fmt = format!("https://oauth.reddit.com/r/{}/.json", self.subreddit);
-        let url = reqwest::Url::parse(&url_fmt).unwrap();
-
-        let header_auth = format!(
+    pub fn get_bearer_token(&self) -> String {
+        format!(
             "Bearer {}",
             self.access_token.as_ref().unwrap().access_token
-        );
+        )
+    }
 
-        let client = reqwest::blocking::Client::new();
-        let res = client
-            .get(url)
-            .header(reqwest::header::USER_AGENT, &self.user_agent)
+    pub fn get_access_token_url(&self) -> String {
+        format!(
+            "https://www.reddit.com/api/v1/access_token?grant_type=password&username={}&password={}",
+            self.user, self.pass
+        )
+    }
+
+    pub fn get_subreddit_url(&self) -> String {
+        format!("https://oauth.reddit.com/r/{}/.json", self.subreddit)
+    }
+
+    pub fn get_paginated_url(&self, after: &String) -> String {
+        format!(
+            "https://oauth.reddit.com/r/{}/.json?after={}",
+            self.subreddit, after
+        )
+    }
+}
+
+async fn check_sleep(reddit_ctx: &Arc<RwLock<Reddit>>) {
+    let remaining_requests = reddit_ctx.read().remaining_requests;
+    let reset_seconds = reddit_ctx.read().reset_seconds;
+
+    println!(
+        "Remaining requests: {} | Reset seconds: {}",
+        remaining_requests, reset_seconds
+    );
+
+    if remaining_requests == 0 {
+        println!("Sleeping for {} seconds...", reset_seconds);
+        std::thread::sleep(std::time::Duration::from_secs(reset_seconds as u64));
+    }
+}
+
+fn parse_headers(reddit_ctx: &Arc<RwLock<Reddit>>, response: &reqwest::Response) {
+    reddit_ctx.write().remaining_requests = response
+        .headers()
+        .get("x-ratelimit-remaining")
+        .unwrap_or(&reqwest::header::HeaderValue::from_str("0.0").unwrap())
+        .to_str()
+        .unwrap_or("0.0")
+        .to_string()
+        .parse::<f32>()
+        .unwrap_or(0.0) as i32;
+
+    reddit_ctx.write().used_requests = response
+        .headers()
+        .get("x-ratelimit-used")
+        .unwrap_or(&reqwest::header::HeaderValue::from_str("0").unwrap())
+        .to_str()
+        .unwrap_or("0")
+        .to_string()
+        .parse::<i32>()
+        .unwrap_or(0);
+
+    reddit_ctx.write().reset_seconds = response
+        .headers()
+        .get("x-ratelimit-reset")
+        .unwrap_or(&reqwest::header::HeaderValue::from_str("0").unwrap())
+        .to_str()
+        .unwrap_or("0")
+        .to_string()
+        .parse::<i32>()
+        .unwrap_or(0);
+}
+
+pub async fn req_threads(
+    reddit_ctx: &Arc<RwLock<Reddit>>,
+    outdir: &String,
+    thread_list: Vec<String>,
+) {
+    let header_auth = reddit_ctx.read().get_bearer_token();
+
+    for thread in thread_list {
+        get_thread(reddit_ctx, outdir, &thread, &header_auth).await;
+    }
+}
+
+async fn get_thread(
+    reddit_ctx: &Arc<RwLock<Reddit>>,
+    outdir: &String,
+    thread_name: &String,
+    header_auth: &String,
+) {
+    {
+        check_sleep(&reddit_ctx).await;
+    }
+
+    let url_fmt = format!("https://oauth.reddit.com{}.json", thread_name);
+    let url = reqwest::Url::parse(&url_fmt).unwrap();
+
+    let user_agent: String;
+    {
+        user_agent = reddit_ctx.read().user_agent.clone();
+    }
+
+    let client = reqwest::Client::new();
+    let res = client
+        .get(url.clone())
+        .header(reqwest::header::USER_AGENT, user_agent)
+        .header(reqwest::header::AUTHORIZATION, header_auth)
+        .send()
+        .await
+        .unwrap();
+
+    println!("Downloaded {}", url);
+
+    let full_path = generate_full_path(outdir.clone(), get_thread_name(&thread_name));
+    println!("Full path: {}", full_path);
+
+    parse_headers(&reddit_ctx, &res);
+
+    let response_json: serde_json::Value = res.json().await.unwrap();
+    let pretty_printed = serde_json::to_string_pretty(&response_json).unwrap();
+
+    save_to_file(&full_path, &pretty_printed);
+}
+
+pub async fn req_access_token(reddit_ctx: &Arc<RwLock<Reddit>>) {
+    let res: reqwest::Response;
+    {
+        let url_fmt = reddit_ctx.read().get_access_token_url();
+        let url = reqwest::Url::parse(&url_fmt).unwrap();
+
+        let user_agent = &reddit_ctx.read().user_agent;
+        let header_auth = reddit_ctx.read().get_basic_token();
+
+        let client = reqwest::Client::new();
+        res = client
+            .post(url)
+            .header(reqwest::header::USER_AGENT, user_agent)
             .header(reqwest::header::AUTHORIZATION, &header_auth)
             .send()
+            .await
+            .unwrap();
+    }
+    let json = res.json::<AccessToken>().await.unwrap();
+
+    reddit_ctx.write().access_token = Some(json.clone());
+}
+
+pub async fn req_subreddit(reddit_ctx: &Arc<RwLock<Reddit>>) -> Vec<String> {
+    let mut thread_url_list: Vec<String> = vec![];
+
+    check_sleep(&reddit_ctx).await;
+
+    let url_fmt = reddit_ctx.read().get_subreddit_url();
+    let url = reqwest::Url::parse(&url_fmt).unwrap();
+
+    let header_auth = reddit_ctx.read().get_bearer_token();
+    let user_agent = &reddit_ctx.read().user_agent;
+
+    let client = reqwest::Client::new();
+    let res = client
+        .get(url)
+        .header(reqwest::header::USER_AGENT, user_agent)
+        .header(reqwest::header::AUTHORIZATION, &header_auth)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let page = serde_json::from_str::<JsonPage>(&res).unwrap();
+    let mut after = page.data.after;
+
+    for permalink in page.data.children {
+        thread_url_list.push(permalink.data.permalink.clone());
+    }
+
+    while after.is_some() {
+        check_sleep(&reddit_ctx).await;
+
+        let next_url_fmt = reddit_ctx.read().get_paginated_url(&after.unwrap());
+        let next_url = reqwest::Url::parse(&next_url_fmt).unwrap();
+
+        let next_client = reqwest::Client::new();
+        let next_res = next_client
+            .get(next_url)
+            .header(reqwest::header::USER_AGENT, user_agent)
+            .header(reqwest::header::AUTHORIZATION, &header_auth)
+            .send()
+            .await
             .unwrap()
             .text()
+            .await
             .unwrap();
 
-        let page = serde_json::from_str::<JsonPage>(&res).unwrap();
-        let mut after = page.data.after;
+        let next_page = serde_json::from_str::<JsonPage>(&next_res).unwrap();
+        after = next_page.data.after;
 
-        for permalink in page.data.children {
+        for permalink in &next_page.data.children {
+            println!("Saving permalink: {}", permalink.data.permalink);
             thread_url_list.push(permalink.data.permalink.clone());
         }
 
-        while after.is_some() {
-            self.check_sleep();
-
-            let next_url_fmt = format!(
-                "https://oauth.reddit.com/r/{}/.json?after={}",
-                self.subreddit,
-                after.unwrap()
-            );
-            let next_url = reqwest::Url::parse(&next_url_fmt).unwrap();
-
-            let next_client = reqwest::blocking::Client::new();
-            let next_res = next_client
-                .get(next_url)
-                .header(reqwest::header::USER_AGENT, &self.user_agent)
-                .header(reqwest::header::AUTHORIZATION, &header_auth)
-                .send()
-                .unwrap()
-                .text()
-                .unwrap();
-
-            let next_page = serde_json::from_str::<JsonPage>(&next_res).unwrap();
-            after = next_page.data.after;
-
-            for permalink in &next_page.data.children {
-                println!("Saving permalink: {}", permalink.data.permalink);
-                thread_url_list.push(permalink.data.permalink.clone());
+        match &after {
+            Some(val) => {
+                println!(
+                    "After: {} Children: {}\n",
+                    val,
+                    next_page.data.children.len()
+                );
             }
-
-            match &after {
-                Some(val) => {
-                    println!(
-                        "After: {} Children: {}\n",
-                        val,
-                        next_page.data.children.len()
-                    );
-                }
-                None => {
-                    break;
-                }
+            None => {
+                break;
             }
         }
-
-        return thread_url_list;
     }
+
+    return thread_url_list;
 }
 
 fn get_thread_name(permalink: &String) -> String {
@@ -341,18 +394,21 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     let r_pass = env::var("REDDIT_PASS").unwrap();
     let r_useragent = env::var("REDDIT_USERAGENT").unwrap();
 
-    let mut reddit_ctx = Reddit::new(
+    let mut reddit_ctx = Arc::new(RwLock::new(Reddit::new(
         r_id,
         r_secret,
         r_user,
         r_pass,
         r_useragent,
         arg_subreddit.to_string(),
-    );
+    )));
 
-    reddit_ctx.req_access_token();
-    let thread_list = reddit_ctx.req_subreddit();
-    reddit_ctx.req_threads(&dir_str, thread_list).await;
+    println!("test0");
+    req_access_token(&reddit_ctx.clone()).await;
+    println!("test1");
+    let thread_list = req_subreddit(&reddit_ctx).await;
+    println!("test2");
+    req_threads(&reddit_ctx, &dir_str, thread_list).await;
 
     Ok(())
 }
